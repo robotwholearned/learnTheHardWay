398d54dc29da024ee693083df2cabaa5a9ee1016
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-02-24 13:54:15 -0700

    ex3 - pre-run

--- ex3.rb
+++ ex3.rb
@@ -1,1 +1,32 @@
 #http://ruby.learncodethehardway.org/book/ex3.html
+#+ plus
+#- minus
+#/ slash
+# * asterick
+# % percent 
+# < less than
+# > greater than
+# <= less than or equal to
+# >= greater than or equal to
+
+puts "I will now count my chickens:"
+
+puts "Hens", 25 + 3 - / 6
+puts "Roosters", 100 -25 * 3 % 4
+
+puts "Now I will count the eggs:" 
+
+puts 3 + 2 + 1 -5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 -7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2?", 3 + 2
+puts "What is 5-7?", 5-7
+
+puts "Oh, that's why it's false."
+
+puts "Is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less than or equal", 5 <= -2
