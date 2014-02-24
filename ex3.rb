04f61cfd3db6f2baa9cc4d19ee51d580e35aa010
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-02-24 15:40:27 -0700

    ex3 - use floats

--- ex3.rb
+++ ex3.rb
@@ -12,28 +12,28 @@
 puts "I will now count my chickens:"
 
 #divides 30 by 6 and adds 25
-puts "Hens", 25 + 30 / 6
+puts "Hens", 25.0 + 30.0 / 6.0
 #it multiplies, mods and then subtracts
 #(100 -((25 * 3) % 4))
-puts "Roosters", 100 - 25 * 3 % 4
+puts "Roosters", 100.0 - 25.0 * 3.0 % 4.0
 
 puts "Now I will count the eggs:" 
 # mod, divide, then left to right add/subtract
 #3 +2 +1 -5 +  (4 % 2) - (1 / 4) + 6
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 puts "Is it true that 3 + 2 < 5 -7?"
 # (3+2) < (5-7)
-puts 3 + 2 < 5 - 7
+puts 3.0 + 2.0 < 5.0 - 7.0
 
-puts "What is 3 + 2?", 3 + 2
-puts "What is 5 - 7?", 5-7
+puts "What is 3 + 2?", 3.0 + 2.0
+puts "What is 5 - 7?", 5.0-7.0
 
 puts "Oh, that's why it's false."
 
 #is five a bigger number than -2?
-puts "Is it greater?", 5 > -2
+puts "Is it greater?", 5.0 > -2.0
 #is 5 bigger or equal to -2?
-puts "Is it greater or equal?", 5 >= -2
+puts "Is it greater or equal?", 5.0 >= -2.0
 #is 5 smaller or equal to -2?
-puts "Is it less than or equal", 5 <= -2
+puts "Is it less than or equal", 5.0 <= -2.0
