23bc2293fe9a5a1650656564d615abf7c952fe33
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-02-24 15:37:42 -0700

    ex3.1 - exra credit

---
+++ ex3.1.rb
@@ -0,0 +1,10 @@
+#Extra Credit
+#http://ruby.learncodethehardway.org/book/ex3.html
+puts "What I make an hour (before taxes) at GT"
+puts (((62000 / 12) / 4) /40)
+puts "What I made an hour (before taxes) at CM"
+puts (((50000 / 12) / 4) /40)
+puts "What Tyler makes an hour (before taxes) at CM"
+puts (((50000 / 12) / 4) /40)
+puts "What Nathan makes an hour (before taxes) at Markit"
+puts (((86000 / 12) / 4) /40)
