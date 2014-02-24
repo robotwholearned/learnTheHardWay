49053bf367c9c7e6d481f96767790c85f6cd52e3
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-02-24 14:53:57 -0700

    Add order precendence table

--- ex3.rb
+++ ex3.rb
@@ -11,7 +11,9 @@
 
 puts "I will now count my chickens:"
 
+#divides 30 by 6 and adds 25
 puts "Hens", 25 + 30 / 6
+#
 puts "Roosters", 100 -25 * 3 % 4
 
 puts "Now I will count the eggs:" 
@@ -30,3 +32,29 @@ puts "Oh, that's why it's false."
 puts "Is it greater?", 5 > -2
 puts "Is it greater or equal?", 5 >= -2
 puts "Is it less than or equal", 5 <= -2
+puts ""
+puts ""
+puts ""
+puts "Operator Precedence Table"
+puts "-----------------------"
+puts "[ ] [ ]=             -> Element Reference, element set"
+puts "**                   -> exponentiation"
+puts "! ~ + -              -> not, complement, unary plus +@, unary minus -@"
+puts "* / %                -> multiply, divide, modulo"
+puts "+ -                  -> Addition and subtraction"
+puts " >> <<	             -> Right and left bitwise shift"
+puts "&	             -> Bitwise `AND'"
+puts "^ |                  -> Bitwise exclusive `OR' and regular `OR'"
+puts "<= < > >=	     -> Comparison operators"
+puts "<=> == === != =~ !~  -> Equality and pattern match operators"
+puts "# &&                 -> Logical `AND'"
+puts "||	             -> Logical `AND'"
+puts ".. ...	             -> Range (inclusive and exclusive)"
+puts "? :                  -> Ternary if-then-else"
+puts "<=> == === != =~ !~ = %= { /= -= += |= &= += |= &= >>= <<= *= &&= ||= **="
+puts "                     -> Assignment"
+puts "defined?	     ->Check if specified symbol defined "
+puts "not                  -> Logical negation"
+puts "or and	             -> Logical composition"
+puts "if unless while until-> Expression modifiers"
+puts "begin/end            -> Block expression"
