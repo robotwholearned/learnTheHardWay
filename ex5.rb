5707a43845d80abeaa70d0acaefe79a7c31722c2
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-02-26 15:01:42 -0700

    ex5 - exra credit - add metric conversions

--- ex5.rb
+++ ex5.rb
@@ -6,10 +6,16 @@ weight = 180 # pounds
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
+#1 inch = 2.54 cm
+#1 pound = 0.453592 kilos
+metric_height = height*2.54
+metric_weight = weight*0.453592
 
 puts "Lets talk about %s." % name
 puts "He's %s inches tall." % height
+puts "He's %s cm tall." % metric_height
 puts "He's %s pounds heavy." % weight
+puts "He's %s kilos heavy." % metric_weight
 puts "Actually that's not too heavy."
 puts "He's got %s eyes and %s hair." %[eyes, hair]
 puts "His teeth are usually %s, depending on the coffee" % teeth
