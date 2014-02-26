cf0a7aaca127ecd7bc4d5e89318233e97c102352
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-02-26 14:50:21 -0700

    ex5 - exra credit - remove my_ from variable names

--- ex5.rb
+++ ex5.rb
@@ -1,17 +1,17 @@
 #http://ruby.learncodethehardway.org/book/ex5.html
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie
-my_height = 74 # inches
-my_weight = 180 # pounds
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # pounds
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
 
-puts "Lets talk about %s." % my_name
-puts "He's %s inches tall." % my_height
-puts "He's %s pounds heavy." % my_weight
+puts "Lets talk about %s." % name
+puts "He's %s inches tall." % height
+puts "He's %s pounds heavy." % weight
 puts "Actually that's not too heavy."
-puts "He's got %s eyes and %s hair." %[my_eyes, my_hair]
-puts "His teeth are usually %s, depending on the coffee" % my_teeth
+puts "He's got %s eyes and %s hair." %[eyes, hair]
+puts "His teeth are usually %s, depending on the coffee" % teeth
 
-puts "If I add %d, %d, and %d I get %d." %[my_age, my_height, my_weight, my_age + my_height + my_weight]
+puts "If I add %d, %d, and %d I get %d." %[age, height, weight, age + height + weight]
