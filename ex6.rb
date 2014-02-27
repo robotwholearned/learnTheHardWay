530af64ce45544054e04716020fa8167085ac47d
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-02-27 11:01:56 -0700

    ex6 - pre-run + post-run

---
+++ ex6.rb
@@ -0,0 +1,27 @@
+#http://ruby.learncodethehardway.org/book/ex6.html
+puts"Exercise 6"
+name1 = "Joe"
+name2 = "Mary"
+puts "Hello %s, where is %s?" % [name1, name2]
+#vs
+name1 = "Joe"
+name2 = "Mary"
+puts "Hello #{name1}, where is #{name2}?"
+
+x = "there are #{10} types of people"
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+puts x
+puts y
+puts "I said #{x}."
+puts "I also said: #{y}."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny? #{hilarious}"
+puts joke_evaluation
+
+w = "This is the left side of . . . "
+e = "a string with a right side."
+
+puts w + e
