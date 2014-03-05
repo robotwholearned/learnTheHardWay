f97e6ce6e4bc03d6982dc178d3edad23122d6d9e
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-03-05 11:52:05 -0700

    ex10 - pre-run

--- 1/ex10.rb
+++ 1/ex10.rb
@@ -1,1 +1,18 @@
 #http://ruby.learncodethehardway.org/book/ex10.html
+puts
+tabby_cat ="\tI'm tabbed in."
+persian_cat = "I'm split \non a line."
+backslash_cat ="I'm \\ a \\ cat"
+
+fat_cat <<FATCAT 
+I'll do a list
+\t* Cat Food
+\t* Fishies
+\t* Catnip\n\t* Grass
+FATCAT
+
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
