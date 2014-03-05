29b0c013b0c5a0c6d45d456dee66ee3763da34bb
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-03-05 11:52:32 -0700

    ex10 - post-run

--- 1/ex10.rb
+++ 1/ex10.rb
@@ -4,7 +4,7 @@ tabby_cat ="\tI'm tabbed in."
 persian_cat = "I'm split \non a line."
 backslash_cat ="I'm \\ a \\ cat"
 
-fat_cat <<FATCAT 
+fat_cat = <<FATCAT 
 I'll do a list
 \t* Cat Food
 \t* Fishies
@@ -16,3 +16,14 @@ puts tabby_cat
 puts persian_cat
 puts backslash_cat
 puts fat_cat
+
+
+# 	I'm tabbed in.
+# I'm split 
+# on a line.
+# I'm \ a \ cat
+# I'll do a list
+# 	* Cat Food
+# 	* Fishies
+# 	* Catnip
+# 	* Grass
