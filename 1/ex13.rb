20cbf4b1e946bb2cfb66d9968695c4c9654ccfe4
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-03-05 13:13:53 -0700

    ex13 - pre-run

--- 1/ex13.rb
+++ 1/ex13.rb
@@ -1,1 +1,10 @@
 #http://ruby.learncodethehardway.org/book/ex13.html
+#"Take whatever is in ARGV, unpack it, and assign it to all of these variables on the left in order."
+first, second, third = ARGV
+
+puts "This script is called #{ $0 }"
+puts "Your first variable is #{first}"
+puts "Your second variable is #{second}"
+puts "Your third variable is #{third}"
+
+
