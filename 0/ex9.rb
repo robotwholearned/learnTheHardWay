dc836642a0f8722d7810975cd28e5a1463491765
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-03-05 11:21:43 -0700

    ex9 - pre-run

--- 0/ex9.rb
+++ 0/ex9.rb
@@ -1,1 +1,14 @@
 #http://ruby.learncodethehardway.org/book/ex9.html
+puts
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: ", days
+puts "Hera are the months: ", months
+
+puts << PARAGRAPH
+There's something going on here.
+With the PARAGRAPH thing.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+PARAGRAPH
