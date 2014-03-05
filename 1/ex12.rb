8a9499a1acde1e17de158e02bb65d4e6c197df3d
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-03-05 12:50:32 -0700

    ex12 - pre-run

--- 1/ex12.rb
+++ 1/ex12.rb
@@ -1,1 +1,13 @@
 #http://ruby.learncodethehardway.org/book/ex12.html
+
+#how to declare a library requirement
+require 'open-uri'
+
+open("http://www.ruby-lang.org/en") do |f|
+	f.each_line {|line| p line}
+	puts f.base_uri
+	puts f.content_type
+	puts f.charset
+	puts f.content_encoding
+	puts f.last_modified
+end
