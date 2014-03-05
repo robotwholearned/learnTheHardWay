ef823ff661046b70e9f23ed13ffa71dd940c1cd9
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-03-05 13:39:54 -0700

    ex14 - pre-run

--- 1/ex14.rb
+++ 1/ex14.rb
@@ -1,1 +1,25 @@
 #http://ruby.learncodethehardway.org/book/ex14.html
+
+user = ARGV.first
+prompt = '> '
+
+puts "Hi #{user}, i'm the #{$0} script."
+puts "I'd like to ask you a few questions."
+
+puts "Do you like me #{user}?"
+print prompt
+likes = STDIN.gets.chomp()
+
+print "Where do you live #{user}?"
+print prompt
+lives = STDIN.gets.chomp()
+
+print "What kind of computer do you have?"
+print prompt
+comp = STDIN.gets.chomp()
+
+puts <<MESSAGE
+	Alright, so you said #{likes} about liking me. 
+	You live in #{lives}. Not sure where that is. 
+	And you have a #{comp} computer. Nice.
+MESSAGE
