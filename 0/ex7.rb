d13b3b4ea94dd88e93da279e76f09e188cacde66
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-03-05 11:03:54 -0700

    Extra credit comments

--- 0/ex7.rb
+++ 0/ex7.rb
@@ -22,3 +22,9 @@ print end1 + end2 + end3 + end4 + end5 + end6
 print end7 + end8 + end9 +end10 +end11 +end12
 
 puts
+
+#  Go back through and write a comment on what each line does.
+# Read each one backwards or out loud to find your errors.
+# From now on, when you make mistakes write down on a piece of paper what kind of mistake you made.
+# When you go to the next exercise, look at the last mistakes you made and try not to make them in this new one.
+# Remember that everyone makes mistakes. Programmers are like magicians who like everyone to think they are perfect and never wrong, but it's all an act. They make mistakes all the time.
