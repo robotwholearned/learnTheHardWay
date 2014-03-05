64fe5b92609a8d516d7314f9a0574ad5db9699f4
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-03-05 13:23:20 -0700

    ex13 - extra-credit

--- 1/ex13.rb
+++ 1/ex13.rb
@@ -1,10 +1,28 @@
 #http://ruby.learncodethehardway.org/book/ex13.html
-#"Take whatever is in ARGV, unpack it, and assign it to all of these variables on the left in order."
+
+# "Take whatever is in ARGV, unpack it, and assign it to all of these variables on the left in order."
 first, second, third = ARGV
 
 puts "This script is called #{ $0 }"
-puts "Your first variable is #{first}"
-puts "Your second variable is #{second}"
-puts "Your third variable is #{third}"
+puts "Your first variable is #{first}, Your second variable is #{second}, Your third variable is #{third}."
+puts" What did you say?"
+firstOne = STDIN.gets.chomp()
+puts"what?"
+secondOne = STDIN.gets.chomp()
+
+if firstOne == secondOne
+	puts "Oooh you said #{firstOne}"
+else
+	puts"Tricky Dicky, you said: \n\t#{firstOne} \nand then said: \n\t#{secondOne}"
+end
 
 
+#Without a matching argument, the variable is empty, []
+
+# name, all, the, args = ARGV
+
+# puts "#{name} #{all} #{the} #{args}"
+
+# name, all = ARGV
+
+# puts "#{name} #{all}"
