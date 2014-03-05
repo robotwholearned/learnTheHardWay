5cf8e124548614a6663338e0502c897b8f8116ce
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-03-05 12:14:16 -0700

    ex11 - post-run

--- 1/ex11.rb
+++ 1/ex11.rb
@@ -2,7 +2,7 @@
 puts
 print "How old are you? "
 age = gets.chomp()
-print = "How tall are you? "
+print "How tall are you? "
 height = gets.chomp()
 print "How much do you weigh? "
 weight = gets.chomp()
