56cd8b37d5f4afc67c671a32323d8f75e71ab13a
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-03-05 11:24:37 -0700

    ex9 - post-run

--- 0/ex9.rb
+++ 0/ex9.rb
@@ -6,7 +6,7 @@ months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 puts "Here are the days: ", days
 puts "Here are the months: ", months
 
-puts << PARAGRAPH
+puts <<PARAGRAPH
 There's something going on here.
 With the PARAGRAPH thing
 We'll be able to type as much as we like.
