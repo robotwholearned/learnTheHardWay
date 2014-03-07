e388cdb86bd18cc90ea60eb3cf6999f5cbdc262c
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-03-07 15:10:35 -0700

    ex16 - pre-run

--- 1/ex16.rb
+++ 1/ex16.rb
@@ -1,1 +1,16 @@
 #http://ruby.learncodethehardway.org/book/ex16.html
+
+# close -- Closes the file. Like File->Save.. in your editor.
+# read -- Reads the contents of the file, you can assign the result to a variable.
+# readline -- Reads just one line of a text file.
+# truncate -- Empties the file, watch out if you care about the file.
+# write(stuff) -- Writes stuff to the file.
+
+filename = ARGV.first
+script = $0
+
+puts "We are going to erase #{filename}"
+puts "If you don't want that, then hit CTRL + C (^C)."
+puts "If you do want to do that, hit RETURN"
+print "? "
+STDIN.gets
