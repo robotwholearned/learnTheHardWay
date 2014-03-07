c35526916e17358a16a61086c34c19b6df7e3760
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-03-07 14:50:03 -0700

    ex15 - pre-run

--- 1/ex15.rb
+++ 1/ex15.rb
@@ -1,1 +1,15 @@
 #http://ruby.learncodethehardway.org/book/ex15.html
+
+filename = ARGV.first
+
+prompt = "> "
+txt = File.open(filename)
+
+puts "Here's your file: #{{filename}}"
+puts txt.read()
+
+puts "I'll also ask you to type it in again:"
+print prompt
+file_again = STDIN.gets.chomp()
+txt_again = File.open(file_again)
+puts txt_again.read()
